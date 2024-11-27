6c984a24f68457f35ed76bec8f91119fb3840138
sivlvy <vladislavsidenko@ukr.net>
2024-11-27 15:47:59 +0200

    added setup project for eslint&prettier

---
+++ src/components/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Route } from 'react-router-dom'
+
+import './App.css'
+
+function App() {
+  return (
+    <React.Fragment>
+      <h1>Hello</h1>
+      <Route />
+    </React.Fragment>
+  )
+}
+
+export default App
