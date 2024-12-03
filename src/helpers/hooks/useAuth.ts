9082bd19111a8c7f33a4f0b956e5aa1ef3d902af
sivlvy <vladislavsidenko@ukr.net>
2024-12-03 15:55:09 +0200

    added signUp/signIn options to redux & updated .env

--- src/helpers/hooks/useAuth.ts
+++ src/helpers/hooks/useAuth.ts
@@ -3,14 +3,12 @@ import { RootState } from '../../redux/store.ts'
 import { useAppSelector } from './useAppSelector.ts'
 
 export const useAuth = () => {
-  const { user } = useAppSelector((state: RootState) => state.auth)
-  if (!user) {
-    return false
-  }
+  const user = useAppSelector((state: RootState) => state.auth.user)
+
   return {
-    isAuth: !!user.email,
-    email: user.email,
-    token: user.token,
-    id: user.id
+    isAuth: !!user?.email,
+    email: user?.email,
+    token: user?.token,
+    id: user?.id
   }
 }
