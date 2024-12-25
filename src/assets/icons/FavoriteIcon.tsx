0a7eb4939deda1e7f7b03c8ded3ad90e908a464e
Vladyslav Hnatok <141771169+VladyslavHn@users.noreply.github.com>
2024-12-25 20:44:44 +0100

    Update src/assets/icons/FavoriteIcon.tsx
    Co-authored-by: sivlvy <vladislavsidenko@ukr.net>

--- src/assets/icons/FavoriteIcon.tsx
+++ src/assets/icons/FavoriteIcon.tsx
@@ -4,7 +4,7 @@ interface Props {
   size?: string | number
   color?: string
   className?: string
-  onClick?: () => void
+  onClick: () => void
 }
 
 const FavoriteIcon: FC<Props> = ({ size = 24, color = '#000', className }) => {
